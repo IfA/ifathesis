155947dabfc7f8cef6b8caf2b5dfca17b7d627ff
shennig <shennig@d0e5e82a-6140-0410-9dd9-c285395373e6>
2011-09-02 11:24:34 +0000

    changed encoding to UTF-8
    git-svn-id: https://eats3.et.tu-dresden.de/svn/projects/de.tud.et.ifa.latex.ifathesis/trunk@5507 d0e5e82a-6140-0410-9dd9-c285395373e6

--- example_files/HelloWorld.java
+++ example_files/HelloWorld.java
@@ -4,10 +4,10 @@ public class HelloWorld {
 	
 	/** 
 	 *	Das ist die Main-Methode in Java; sie gilt als 
-	 *	Einsprungpunkt für die Virtual Machine
+	 *	Einsprungpunkt fÃ¼r die Virtual Machine
 	 *  
 	 *	@param args Liste der Parameter, die beim Start des 
-	 *				Programms übergeben wurden
+	 *				Programms Ã¼bergeben wurden
 	 *	@author Stefan Hennig, 2010
 	 */
 	public static void main(String[] args) {
